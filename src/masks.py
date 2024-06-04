f7e855f21ed73fb9d40a52aad80c6fd5dcfae213
A1 <F5654521@yandex.ru>
2024-06-05 02:26:56 +0900

    add project

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def mask_card(card_numb: str) -> str:
+    """Return masked card(str)"""
+    return f"{card_numb[:5]} {card_numb[5:7]}{"*" * 2} {"*" * 4} {card_numb[12:]}"
+
+
+def mask_account(account_number: str) -> str:
+    """Return masked account number(str)"""
+    return f"{'*' * 2}{account_number[-4::]}"
