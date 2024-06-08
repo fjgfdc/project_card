ad89fd810b43b2bc9dc4214ce61f79db57f4188d
A1 <F5654521@yandex.ru>
2024-06-09 01:27:40 +0900

    fixes functions in processing

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,16 @@
-def filter_by_state(state_list: list) -> list:
+from typing import Iterable
+
+
+def filter_by_state(state_list: Iterable, normal_state: str = "EXECUTED") -> Iterable:
     """Return filtered list by state"""
+    total_list = []
+    for dict_list in state_list:
+        if dict_list.get("state") == normal_state:
+            total_list.append(dict_list)
+    return total_list
 
 
-def sort_by_date(date_list: list) -> list:
+def sort_by_date(date_list: Iterable, reverse_list: bool = True) -> Iterable:
     """Return filtered list by date"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
+    return sorted_list
