a19040da4bb5a6cb0a69a2e5280efb80b7056c11
A1 <F5654521@yandex.ru>
2024-06-11 19:53:23 +0900

    добавлены импорты

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,6 +1,5 @@
 from src.widget import mask, date
+from src.processing import filter_by_state, filter_by_date
+from tests import conftest
 
 
-"""prints for tests"""
-print(mask("Maestro 1596837868705199"))
-print(date("2018-07-11T02:26:18.671407"))
