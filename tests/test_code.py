8e4c8d26e05f59030ea60a5bac099078b3c14a45
A1 <F5654521@yandex.ru>
2024-06-11 22:53:47 +0900

    добавлены функции для проверки

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,5 +1,15 @@
 from src.widget import mask, date
-from src.processing import filter_by_state, filter_by_date
+from src.processing import filter_by_state, sort_by_date
 from tests import conftest
 
 
+def test_mask():
+
+
+def test_date():
+
+
+def test_filter_by_state():
+
+
+def test_sort_by_date():
