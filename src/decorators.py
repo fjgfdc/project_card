cb6eb10722d5dccd2eae63e135ce44dd7858ae4b
A1 <F5654521@yandex.ru>
2024-06-21 02:50:44 +0900

    закончена работа в декораторах

---
+++ src/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+
+def log(filename=""):
+    def log_wrapper(func):
+        @wraps(func)
+        def log_inner(*args, **kwargs):
+            try:
+                massage = f"{func.__name__} ok"
+                result = func(*args, **kwargs)
+                if not filename:
+                    print(f"{massage}")
+                else:
+                    with open(filename, "w") as file:
+                        file.write(f"{massage}\n")
+                return result
+            except Exception as e:
+                massage_error = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
+                if not filename:
+                    print(f"{massage_error}")
+                else:
+                    with open(filename, "w") as file:
+                        file.write(f"{massage_error}\n")
+        return log_inner
+    return log_wrapper
+
+
+@log(filename="../mylog.txt")
+def my_function(x, y):
+    """Возвращает значение и записывает коректность работы функции в mylog.txt"""
+    return x + y
+
+
+my_function(1, 2)
